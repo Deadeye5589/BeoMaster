e288daf54409bf19e41b488659070777a62ede84
Deadeye5589 <deadeye5589@hotmail.com>
2023-05-11 10:22:41 +0200

    Updated left display to framed design

--- src/ui.h
+++ src/ui.h
@@ -12,34 +12,33 @@ extern "C" {
 
 #include <lvgl.h>
 
-#include "ui_events.h"
-extern lv_obj_t * ui_Screen1;
-extern lv_obj_t * ui_KreisBG;
-extern lv_obj_t * ui_ArcLevel;
-extern lv_obj_t * ui_KreisKnopf;
-extern lv_obj_t * ui_KreisLCD;
-extern lv_obj_t * ui_BoxRechts;
-extern lv_obj_t * ui_varRoller;
-extern lv_obj_t * ui_SourceSelect;
-extern lv_obj_t * ui_varAmpLevel;
-extern lv_obj_t * ui_Dot1;
-extern lv_obj_t * ui_Dot2;
-extern lv_obj_t * ui_Dot3;
-extern lv_obj_t * ui_Dot4;
-extern lv_obj_t * ui_Dot5;
-extern lv_obj_t * ui_Nulldb;
-extern lv_obj_t * ui_Infdb;
-extern lv_obj_t * ui_varTemperatur;
-extern lv_obj_t * ui_GradCelsius;
-extern lv_obj_t * ui_AmpTemp;
-extern lv_obj_t * ui_ImageMute;
-extern lv_obj_t * ui____initial_actions0;
+extern lv_obj_t *ui_Screen1;
+extern lv_obj_t *ui_KreisBG;
+extern lv_obj_t *ui_ArcLevel;
+extern lv_obj_t *ui_KreisKnopf;
+extern lv_obj_t *ui_KreisLCD;
+extern lv_obj_t *ui_varRoller;
+extern lv_obj_t *ui_SourceSelect;
+extern lv_obj_t *ui_varAmpLevel;
+extern lv_obj_t *ui_Dot1;
+extern lv_obj_t *ui_Dot2;
+extern lv_obj_t *ui_Dot3;
+extern lv_obj_t *ui_Dot4;
+extern lv_obj_t *ui_Dot5;
+extern lv_obj_t *ui_Nulldb;
+extern lv_obj_t *ui_Infdb;
+extern lv_obj_t *ui_varTemperatur;
+extern lv_obj_t *ui_GradCelsius;
+extern lv_obj_t *ui_AmpTemp;
+extern lv_obj_t *ui_ImageMute;
+extern lv_obj_t *ui____initial_actions0;
 
 
-LV_IMG_DECLARE(ui_img_mutesymbolscaled_png);    // assets\mutesymbolscaled.png
+LV_IMG_DECLARE( ui_img_display_left_png);   // assets\Display_Left.png
+LV_IMG_DECLARE( ui_img_mutesymbolscaled_png);   // assets\mutesymbolscaled.png
 
 
-LV_FONT_DECLARE(ui_font_Digitaled);
+LV_FONT_DECLARE( ui_font_Digitaled);
 
 
 void ui_init(void);
